304fd20a818c182fa2752f270cebb82e824cda0e
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-06-15 17:56:46 +0900

    [rust-basics]refactor: convert single crate project to workspace with basics binary crate and utils library crate

--- rust-basics/basics/src/main.rs
+++ rust-basics/basics/src/main.rs
@@ -1,5 +1,3 @@
-mod utils;
-
 struct Circle {
     radius: f64,
 }
--- rust-basics/utils/src/lib.rs
+++ rust-basics/utils/src/lib.rs
@@ -1,3 +1,18 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
 use std::f64::consts::PI;
 
 pub fn greet(name: &str) {

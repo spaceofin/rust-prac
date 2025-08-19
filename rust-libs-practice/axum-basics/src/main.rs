8f31137bffabd4af98fc35452132d932175385c0
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-08-19 20:53:07 +0900

    [rust-libs-practice]refactor: move routes.rs to routes/sample_routes.rs

--- rust-libs-practice/axum-basics/src/main.rs
+++ rust-libs-practice/axum-basics/src/main.rs
@@ -1,8 +1,10 @@
 mod routes;
 
+use routes::sample_routes;
+
 #[tokio::main]
 async fn main() {
-    let app = routes::create_router();
+    let app = sample_routes::create_router();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();

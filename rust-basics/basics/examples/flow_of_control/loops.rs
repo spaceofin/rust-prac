bb60671898da930c0886c04e9a761584b05850e9
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-08-30 21:37:50 +0900

    [rust-basics]feat: update loops module for while and for exercises

--- rust-basics/basics/examples/flow_of_control/loops.rs
+++ rust-basics/basics/examples/flow_of_control/loops.rs
@@ -40,6 +40,82 @@ fn loops (){
     println!("result: {}", result);
 }
 
+fn fizz_buzz_while() {
+    let mut n = 1;
+
+    while n < 101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+        n += 1;
+    }
+}
+
+fn fizz_buzz_for() {
+    // for n in 1..=100 { ... } works the same
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+}
+
+fn for_and_iterators() {
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    println!("----------iter----------");
+    // iter: this borrows each element of the collection
+    for name in names.iter() {
+        match name { // &&str
+            &"Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    
+    println!("names: {:?}", names);
+
+    println!("----------into_iter----------");
+    // into_iter: this consumes the collection
+    for name in names.into_iter() {
+        match name { // &str
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    
+    // compile error: the data has already been consumed by into_iter
+    // println!("names: {:?}", names);
+
+    println!("----------iter_mut----------");
+
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+    // iter_mut: this mutably borrows each element of the collection
+    for name in names.iter_mut() {
+        *name = match name { // &mut &str
+            &mut "Ferris" => "There is a rustacean among us!",
+            &mut "Bob" => "Hello",
+            _ => *name,
+        }
+    }
+
+    println!("names: {:?}", names);
+}
+
 pub fn loops_demo() {
-    loops();
+    // loops();
+    // fizz_buzz_while();
+    // fizz_buzz_for();
+    for_and_iterators();
 }

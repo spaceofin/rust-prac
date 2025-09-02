fc41836b5cecf06c12b39a546def473cb5ed1cc4
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-09-02 19:42:14 +0900

    [rust-basics]feat: update conditionals module for pointers/ref destructuring exercises

--- rust-basics/basics/examples/flow_of_control/conditionals.rs
+++ rust-basics/basics/examples/flow_of_control/conditionals.rs
@@ -188,6 +188,57 @@ fn match_destructuring() {
     let bar = Bar { foo: faa };
     let Bar { foo: Foo { x: nested_x, y: nested_y } } = bar;
     println!("Nested: nested_x = {nested_x:?}, nested_y = {nested_y:?}");
+
+    println!("----------pointers/ref----------");
+    
+    // assign reference
+    let reference = &4;
+    match reference {
+        &val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    // To avoid the `&`, you dereference before matching.
+    match *reference {
+        val => println!("Got a value via dereferencing: {:?}", val),
+    }
+
+    // 'ref' modifies the assignment so that a reference is created for the element
+    let ref is_a_reference = 3;
+
+    match is_a_reference {
+        &val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    // values without references
+    let value = 5;
+    let mut mut_value = 6;
+
+    // references can be retrieved via `ref` and `ref mut`.
+    match value {
+        // copied because of Copy trait
+        r => println!("Got a reference to a value: {:?}", r),
+    }
+    match value {
+        ref r => println!("Got a reference to a value: {:?}", r),
+    }
+    match mut_value {
+        // copied because of Copy trait
+        m => {
+            println!("`mut_value`: {:?}", m);
+        },
+    }
+    match mut_value {
+        ref m => {
+            println!("`mut_value`: {:?}", m);
+        },
+    }
+    match mut_value {
+        ref mut m => {
+            // Got a reference. Gotta dereference it before we can add anything to it.
+            *m += 10;
+            println!("We added 10. `mut_value`: {:?}", m);
+        },
+    }
 }
 
 pub fn conditionals_demo() {

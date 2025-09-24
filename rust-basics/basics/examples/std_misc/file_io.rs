6eee053dfb29d7b0d0d24aa79557956e5c841bb3
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-09-24 19:43:59 +0900

    [rust-basics]feat: update file_io module for creating file exercises

--- rust-basics/basics/examples/std_misc/file_io.rs
+++ rust-basics/basics/examples/std_misc/file_io.rs
@@ -2,21 +2,27 @@ use std::fs::File;
 use std::io::{prelude::*, self};
 use std::path::Path;
 
-fn open_example() {
+fn get_base_path() -> &'static Path {
+    let source_file = file!();
+    Path::new(source_file).parent().unwrap()
+}
 
+fn enter_filename() -> String {
     print!("Enter the file name in the files folder: ");
     io::stdout().flush().unwrap();
-
+    
     let mut filename = String::new();
     io::stdin()
         .read_line(&mut filename)
         .expect("Failed to read input");
-    let filename = filename.trim();
+    filename.trim().to_string()
+}
 
-    let source_file = file!();
+fn open_example() {
+    let filename = enter_filename();
     
-    let base_path = Path::new(source_file).parent().unwrap();
-    let path = base_path.join("files").join(filename);
+    let base_path = get_base_path();
+    let path = base_path.join("files").join(&filename);
     let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
@@ -35,6 +41,42 @@ fn open_example() {
     // `file` goes out of scope, and the "hello.txt" file gets closed
 }
 
+static LOREM_IPSUM: &str =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+";
+
+fn create_example() {
+    let base_path = get_base_path();
+    let filename = enter_filename();
+
+    let path = base_path.join("files").join(&filename);
+    let display = path.display();
+
+    if path.exists() { 
+        println!("File {} already exists. It will be overwritten.", &filename); 
+    } else { 
+    println!("Creating a new file: {}", &filename);
+    }
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+    match file.write_all(LOREM_IPSUM.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+}
+
 pub fn file_io_demo() {
-    open_example();
+    // open_example();
+    create_example();
 }

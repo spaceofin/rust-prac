04e11da2f612a0cc62c78c0a02fd0c4a808aa8e4
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-10-29 12:30:40 +0900

    [rust-basics]feat: update unit_testing module for unit testing exercises

--- rust-basics/basics/examples/testing/runner.rs
+++ rust-basics/basics/examples/testing/runner.rs
@@ -1,6 +1,1 @@
-use super::unit_testing;
-
-pub fn run () {
-    unit_testing::unit_testing_demo();
-
-}
+pub fn run () {}
--- rust-basics/basics/examples/testing/unit_testing.rs
+++ rust-basics/basics/examples/testing/unit_testing.rs
@@ -11,6 +11,15 @@ fn sqrt(number: f64) -> Result<f64, String> {
     }
 }
 
+fn divide_non_zero_result(a: u32, b: u32) -> u32 {
+    if b == 0 {
+        panic!("Divide-by-zero error");
+    } else if a < b {
+        panic!("Divide result is zero");
+    }
+    a / b
+}
+
 #[cfg(test)]
 mod tests {
     // Note thie useful idiom: importing names from outer (for mod tests) scope.
@@ -36,8 +45,50 @@ mod tests {
 
         Ok(())
     }
-}
 
-pub fn unit_testing_demo() {
+        #[test]
+    fn test_divide() {
+        assert_eq!(divide_non_zero_result(10, 2), 5);
+    }
 
+    #[test]
+    #[should_panic]
+    fn test_divide_by_zero_panic() {
+        divide_non_zero_result(1, 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Divide-by-zero error")]
+    fn test_divide_by_zero_panic_expected() {
+        divide_non_zero_result(1, 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Divide by zero error")]
+    fn test_divide_by_zero_panic_wrong_expected() {
+        divide_non_zero_result(1, 0);
+    }
+
+    #[should_panic(expected = "Divide result is zero")]
+    fn test_devide_result_zero_panic() {
+        divide_non_zero_result(1, 10);
+    }
+
+    #[test]
+    #[should_panic(expected = "result")]
+    fn test_devide_result_zero_panic_substring() {
+        divide_non_zero_result(1, 10);
+    }
+
+    #[test]
+    #[should_panic = "Divide result is zero"] 
+    fn test_devide_result_zero_panic_shorthand() {
+        divide_non_zero_result(1, 10);
+    }
+
+    #[test]
+    #[ignore]
+    fn ignored_test() {
+        assert_eq!(add(0, 0), 0);
+    }
 }

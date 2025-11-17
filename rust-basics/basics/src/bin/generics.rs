31b8a5adeb3ed7672887cb6c118be8df92df143f
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-11-17 17:45:06 +0900

    [rust-basics]feat: add generics binary for generics exercises

---
+++ rust-basics/basics/src/bin/generics.rs
@@ -0,0 +1,98 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_assignments)]
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+struct Point1<T> {
+    x: T,
+    y: T,
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T> Point1<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+    fn y(&self) -> &T {
+        &self.y
+    }
+}
+
+impl Point1<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+impl<T, U> Point2<T, U> {
+    fn mixup<V, W>(self, other: Point2<V, W>) -> Point2<T, W> {
+        Point2 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+
+#[derive(Debug)]
+enum Status<T> {
+    Active(T),
+    Inactive,
+}
+
+impl<T> Status<T> {
+    fn unwrap(self) -> T {
+        match self {
+            Status::Active(val) => val,
+            Status::Inactive => panic!("called `unwrap()` on an Inactive value"),
+        }
+    }
+}
+
+fn generics_basic() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {result}");
+
+    let integer = Point1 { x: 5, y: 10 };
+    let float = Point1 { x: 1.0, y: 4.0 };
+
+    println!("integer Point: x = {}, y = {}", integer.x(), integer.y() );
+
+    let both_integer = Point2 { x: 5, y: 10 };
+    let both_float = Point2 { x: 1.0, y: 4.0 };
+    let integer_and_float = Point2 { x: 5, y: 4.0 };
+
+    let p1 = Point2 { x: 5, y: 10.4 };
+    let p2 = Point2 { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+    let score = Status::Active(100);
+    let no_score: Status<i32> = Status::Inactive;
+    println!("score: {:?}, no_score: {:?}", score.unwrap(), no_score);
+}
+
+fn main() {
+    generics_basic();
+}

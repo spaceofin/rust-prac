a01b1ca0924a3291942cf571ae20162148ff4caf
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-11-22 19:35:47 +0900

    [rust-basics]feat: add closures binary for closures exercises

---
+++ rust-basics/basics/src/bin/closures.rs
@@ -0,0 +1,85 @@
+#![allow(dead_code)]
+
+use std::{thread, time::Duration};
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn closures_demo() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+}
+
+fn closures_types() {
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+    let result = expensive_closure(10);
+    println!("caculation result: {}", result);
+
+    fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+    let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    let add_one_v3 = |x| { x + 1 };
+    let add_one_v4 = |x| x + 1;
+
+    println!("add_one_result: {} {} {} {}",
+        add_one_v1(1), add_one_v2(2), add_one_v3(3), add_one_v4(4));
+
+    let example_closure = |x| x;
+    let s = example_closure(String::from("hello"));
+    // Compile Error: the closure has been inferred to take a `String`,
+    // let n = example_closure(5);
+    println!("s: {s}");
+}
+
+fn main() {
+    // closures_demo();
+    closures_types();
+}

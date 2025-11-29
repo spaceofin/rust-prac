25d0c95b9126b130059b96db468a67bea54937ea
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-11-29 16:42:57 +0900

    [rust-basics]feat: update pattern_matching module for loops, function parameters, and refutability exercises

--- rust-basics/basics/src/rust_lang/pattern_matching.rs
+++ rust-basics/basics/src/rust_lang/pattern_matching.rs
@@ -41,6 +41,75 @@ fn let_demo() {
     }
 }
 
+fn loops_demo() {
+    let v = vec!['a', 'b', 'c'];
+
+    for value in v.iter() {
+        print!("{value} ");
+    }
+    println!();
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{value} is at index {index}");
+    }
+    println!("v: {v:?}");
+}
+
+// Function parameters can also be patterns. 
+fn print_tuple_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({x}, {y})");
+}
+
+fn print_individual_coordinates(&x:&i32, &y: &i32) {
+    println!("Current location: ({x}, {y})");
+}
+
+fn fn_parameters_demo() {
+    let point = (3, 5);
+    print_tuple_coordinates(&point);
+    print_individual_coordinates(&point.0, &point.1);
+}
+
+fn refutability_demo() {
+    // let some_option_value: Option<i32> = None;
+    let some_option_value: Option<i32> = Some(10);
+    // Compile Error: `let` bindings require an "irrefutable pattern"
+    // let Some(x) = some_option_value;
+    let Some(x) = some_option_value else {
+        return;
+    };
+
+    // Warning: This pattern will always match, so the `else` clause is useless
+    // let x = 5 else { return; };
+    let x = 5;
+
+    let some_option = Some(5);
+    match some_option {
+        // refutable
+        Some(x) => println!("Found a value: {}", x),
+        // refutable
+        None => println!("No value"),
+    }
+    match some_option {
+        // refutable
+        Some(x) => println!("Found a value: {}", x),
+        // irrefutable
+         _ => println!("Other case"),
+    }
+
+    let x = 10;
+    // This syntax isn’t particularly useful and could be replaced with a simpler let statement.
+    // match x {
+    //     // irrefutable
+    //     n => println!("The value is {}", n),
+    // }
+    let n = x;
+    println!("The value is {}", n);
+}
+
 pub fn run() {
-    let_demo();
+    // let_demo();
+    // loops_demo();
+    // fn_parameters_demo();
+    refutability_demo();
 }

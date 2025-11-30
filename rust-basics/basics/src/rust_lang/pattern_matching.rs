c731dc7796caa21944624a74f7ddd81c750d82a8
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-11-30 16:31:28 +0900

    [rust-basics]feat: update pattern_matching module for pattern syntax exercises

--- rust-basics/basics/src/rust_lang/pattern_matching.rs
+++ rust-basics/basics/src/rust_lang/pattern_matching.rs
@@ -107,9 +107,123 @@ fn refutability_demo() {
     println!("The value is {}", n);
 }
 
+fn pattern_examples() {
+    let a = 1;
+
+    // Matching Literals
+    match a {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // Multiple Patterns
+    match a {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    // Matching Ranges of Values with ..=
+    let aa = 'a';
+    match a {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+    match aa {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    let b = Some(5);
+    let c = 10;
+
+    // Matching Named Variables
+    match b {
+        Some(50) => println!("Got 50"),
+        Some(c) => println!("Matched, c = {c}"),
+        _ => println!("Default case, b = {b:?}"),
+    }
+    println!("at the end: b = {b:?}, c = {c}");
+
+    // Destructuring to Break Apart Values
+    // Destructuring Structs
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let p = Point { x: 0, y: 7 };
+    let Point { x: d, y: e } = p;
+    println!("d: {d:?}, e: {e:?}");
+    let Point { x, y } = p;
+    println!("x: {x:?}, y: {y:?}");
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
+
+    // Destructuring Enums
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColorRGB(i32, i32, i32),
+        ChangeColor(Color),
+    }
+
+    let msg = Message::ChangeColorRGB(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColorRGB(r, g, b) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
+        }
+        _ => (),
+    }
+
+    //Destructuring Nested Structs and Enums
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
+        }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change color to hue {h}, saturation {s}, value {v}");
+        }
+        _ => (),
+    }
+
+    // Destructuring Structs and Tuples
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+    println!("feet: {feet}, inches: {inches}, x: {x}, y: {y}");
+
+}
+
 pub fn run() {
     // let_demo();
     // loops_demo();
     // fn_parameters_demo();
-    refutability_demo();
+    // refutability_demo();
+    pattern_examples();
 }

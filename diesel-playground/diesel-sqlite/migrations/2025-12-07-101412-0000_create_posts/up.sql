f5535a6c220ce227577071aa9b5a30f22e5359ba
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-12-07 19:33:03 +0900

    [diesel-playground]feat: add diesel and create_posts migration

---
+++ diesel-playground/diesel-sqlite/migrations/2025-12-07-101412-0000_create_posts/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    published BOOLEAN NOT NULL DEFAULT 0
+);

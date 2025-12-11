38aa1f68f8116efaa98f65f11064c4a68dbef32e
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-12-11 17:24:11 +0900

    [rust-basics]feat: update smart_pointers module for RefCell exercises

--- rust-basics/basics/src/rust_lang/smart_pointers.rs
+++ rust-basics/basics/src/rust_lang/smart_pointers.rs
@@ -207,10 +207,112 @@ fn rc_examples() {
     println!("reference count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
+trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where T: Messenger,
+{
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+
+        let percentage_of_max = self.value as f64 / self.max as f64;
+
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over tour quota!");
+        } else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used up over 90% of your quota!");
+        } else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // struct MockMessenger {
+    //     sent_messages: Vec<String>,
+    // }
+
+    // impl MockMessenger {
+    //     fn new() -> MockMessenger {
+    //         MockMessenger {
+    //             sent_messages: vec![],
+    //         }
+    //     }
+    // }
+
+    // impl Messenger for MockMessenger {
+    //     fn send(&self, message: &str) {
+    //         // Compile Error: `self` is a `&` reference, so the data it refers to cannot be borrowed as mutable
+    //         // self.sent_messages.push(String::from(message));
+    //     }
+    // }
+
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+            // Runtime panic: multiple mutable borrows on RefCell.
+            // let mut second_borrow = self.sent_messages.borrow_mut();
+            // second_borrow.push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+}
+
+fn ref_cell_examples() {
+    let x = 5;
+    // Compile Error: cannot borrow `x` as mutable, as it is not declared as mutable
+    // let y = &mut x;
+}
+
 pub fn run() {
     // smart_pointers_basics();
     // deref_examples();
     // deref_coercion_examples();
     // drop_examples();
-    rc_examples();
+    // rc_examples();
+    ref_cell_examples();
 }

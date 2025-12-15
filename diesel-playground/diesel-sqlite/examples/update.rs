4f4a13e1bfbacfc14802538a98ad854ab004e53e
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-12-16 00:49:44 +0900

    [diesel-playground]feat: update update binary for AsChangeset exercises

--- diesel-playground/diesel-sqlite/examples/update.rs
+++ diesel-playground/diesel-sqlite/examples/update.rs
@@ -1,8 +1,22 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use diesel::prelude::*;
 use diesel_sqlite::*;
 use self::schema::articles::dsl::*;
 use diesel::{debug_query, sqlite::Sqlite, dsl::now};
-use chrono::NaiveDateTime;
+use chrono::{NaiveDateTime, Utc};
+
+#[derive(Queryable, Identifiable, AsChangeset, Debug)]
+#[diesel(table_name = self::schema::articles)]
+struct Article {
+    id: i32,
+    title: String,
+    body: String,
+    draft: bool,
+    publish_at: NaiveDateTime,
+    visit_count: i32,
+}
 
 fn update_basic(connection: &mut SqliteConnection) {
   let query1 = diesel::update(articles).set(draft.eq(false));
@@ -14,17 +28,6 @@ fn update_basic(connection: &mut SqliteConnection) {
     .set(draft.eq(false));
   // UPDATE `articles` SET `draft` = ? WHERE (`articles`.`publish_at` < CURRENT_TIMESTAMP) -- binds: [false]
   println!("{}", debug_query::<Sqlite, _>(&query2));
-
-  #[derive(Queryable, Identifiable, AsChangeset, Debug)]
-  #[diesel(table_name = self::schema::articles)]
-  pub struct Article {
-      pub id: i32,
-      pub title: String,
-      pub body: String,
-      pub draft: bool,
-      pub publish_at: NaiveDateTime,
-      pub visit_count: i32,
-  }
 
   let article1 = articles.find(1)
     .first::<Article>(connection)
@@ -62,7 +65,62 @@ fn update_basic(connection: &mut SqliteConnection) {
   
 }
 
+fn update_with_aschangeset(connection: &mut SqliteConnection) {
+
+  #[derive(AsChangeset)]
+  #[diesel(table_name = self::schema::articles)]
+  struct ArticleChangeSet {
+      title: String,
+      body: String,
+      draft: bool,
+      publish_at: NaiveDateTime,
+      visit_count: i32,
+  }
+
+  let current_time = Utc::now().naive_utc();
+
+  let article = ArticleChangeSet {
+    title: "".to_string(),
+    body: "".to_string(),
+    draft: false,
+    publish_at: current_time,
+    visit_count: 0,
+  };
+
+  let article_with_id = Article {
+    id: 0,
+    title: "".to_string(),
+    body: "".to_string(),
+    draft: false,
+    publish_at: current_time,
+    visit_count: 0,
+  };
+
+  let query1 = diesel::update(articles).set(article);
+  println!("query1:\n{}", debug_query::<Sqlite, _>(&query1));
+
+  // #[derive(AsChangeset)] automatically ignores the primary key if it is `id`; otherwise, use #[diesel(primary_key(...))].
+  let query2 = diesel::update(articles).set(article_with_id);
+  println!("query2:\n{}", debug_query::<Sqlite, _>(&query2));
+
+  #[derive(AsChangeset)]
+  #[diesel(table_name = self::schema::articles)]
+  struct ArticleForm<'a> {
+      title: Option<&'a str>,
+      body: Option<&'a str>,
+  }
+
+  let query3 = diesel::update(articles)
+    .set(&ArticleForm {
+        title: None,
+        body: Some("My new post"),
+    });
+  println!("query3:\n{}", debug_query::<Sqlite, _>(&query3));
+
+}
+
 fn main() {
   let connection = &mut establish_connection();
-  update_basic(connection);
+  // update_basic(connection);
+  update_with_aschangeset(connection);
 }

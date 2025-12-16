e61ddaf2156d774e98f061e7d2fb15f1e14e00a2
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-12-16 18:38:07 +0900

    [rust-basics]feat: update smart_pointers module for Rc references exercises

--- rust-basics/basics/src/rust_lang/smart_pointers.rs
+++ rust-basics/basics/src/rust_lang/smart_pointers.rs
@@ -506,6 +506,53 @@ fn weak_reference() {
     println!("branch strong_count: {}, weak_count: {}", Rc::strong_count(&branch), Rc::weak_count(&branch));
 }
 
+fn visualize_strong_and_weak_counts() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        println!("-----inner scope start-----");
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+        println!("-----inner scope end-----");
+    }
+    // A non-zero weak count (weak_count >= 1) does not cause memory leaks,
+    // when an Rc value is dropped.
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+}
+
 pub fn run() {
     // smart_pointers_basics();
     // deref_examples();
@@ -516,5 +563,6 @@ pub fn run() {
     // reference_cycle();
     // tri_reference_cycle();
     // weak_reference_basic();
-    weak_reference();
+    // weak_reference();
+    visualize_strong_and_weak_counts();
 }

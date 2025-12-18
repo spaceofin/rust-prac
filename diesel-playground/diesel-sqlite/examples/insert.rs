c8350832c63283230b426307aa1adf0e0cb817bd
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-12-18 16:23:46 +0900

    [diesel-playground]feat: update insert binary for Insertable exercises

--- diesel-playground/diesel-sqlite/examples/insert.rs
+++ diesel-playground/diesel-sqlite/examples/insert.rs
@@ -49,9 +49,46 @@ fn insert_user_with_name_and_haircolor(connection: &mut SqliteConnection, user_n
     println!("inserted_user:\n{inserted_user:?}")
 }
 
+fn insert_with_insertable(connection: &mut SqliteConnection) {
+    use serde::Deserialize;
+    use serde_json;
+    use self::schema::users::dsl::*;
+
+    #[derive(Debug, Deserialize, Insertable)]
+    #[diesel(table_name = self::schema::users)]
+    pub struct UserForm<'a> {
+        name: &'a str,
+        hair_color: Option<&'a str>,
+    }
+
+    // let json = r#"{ "name": "Sean", "hair_color": "Black" }"#;
+    // println!("json:\n{json}");
+    // let user_form = serde_json::from_str::<UserForm>(json).expect("failed to parse UserForm");
+    // println!("user_form:\n{user_form:?}");
+
+    // let inserted_user = diesel::insert_into(users).values(&user_form)
+    //     .get_result::<User>(connection)
+    //     .expect("failed to insert user");
+    // println!("inserted_user:\n{inserted_user:?}");
+
+    //If one of the fields is None, the default value will be inserted for that field.
+    let json = r#"{ "name": "Ruby", "hair_color": null }"#;
+    println!("json:\n{json}");
+    let user_form = serde_json::from_str::<UserForm>(json).expect("failed to parse UserForm");
+    println!("user_form:\n{user_form:?}");
+
+    let insert_user_query = diesel::insert_into(users).values(&user_form);
+    println!("insert user query:\n{}",debug_query::<Sqlite, _>(&insert_user_query));
+    let inserted_user = insert_user_query
+        .get_result::<User>(connection)
+        .expect("failed to insert user");
+    println!("inserted_user:\n{inserted_user:?}");
+}
+
 fn main() {
   let connection = &mut establish_connection();
     // insert_defaults(connection);
     // insert_user_with_name(connection, "Sean");
-    insert_user_with_name_and_haircolor(connection, "Tess", "Brown");
+    // insert_user_with_name_and_haircolor(connection, "Tess", "Brown");
+    insert_with_insertable(connection);
 }

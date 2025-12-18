30cb3733a9654df570f7466c4bf43aa0d998489c
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-12-19 01:58:59 +0900

    [diesel-playground]feat: update insert binary for batch insert exercises

--- diesel-playground/diesel-sqlite/examples/insert.rs
+++ diesel-playground/diesel-sqlite/examples/insert.rs
@@ -5,6 +5,8 @@ use chrono::NaiveDateTime;
 use diesel::prelude::*;
 use diesel_sqlite::*;
 use diesel::{debug_query, sqlite::Sqlite};
+use serde::Deserialize;
+use serde_json;
 
 #[derive(Debug, Queryable)]
 #[diesel(table_name = users)]
@@ -14,6 +16,13 @@ struct User {
     hair_color: Option<String>,
     created_at: NaiveDateTime,
     updated_at: NaiveDateTime
+}
+
+#[derive(Debug, Deserialize, Insertable)]
+#[diesel(table_name = self::schema::users)]
+pub struct UserForm<'a> {
+    name: &'a str,
+    hair_color: Option<&'a str>,
 }
 
 fn insert_defaults(connection: &mut SqliteConnection) {
@@ -50,16 +59,7 @@ fn insert_user_with_name_and_haircolor(connection: &mut SqliteConnection, user_n
 }
 
 fn insert_with_insertable(connection: &mut SqliteConnection) {
-    use serde::Deserialize;
-    use serde_json;
     use self::schema::users::dsl::*;
-
-    #[derive(Debug, Deserialize, Insertable)]
-    #[diesel(table_name = self::schema::users)]
-    pub struct UserForm<'a> {
-        name: &'a str,
-        hair_color: Option<&'a str>,
-    }
 
     // let json = r#"{ "name": "Sean", "hair_color": "Black" }"#;
     // println!("json:\n{json}");
@@ -85,10 +85,41 @@ fn insert_with_insertable(connection: &mut SqliteConnection) {
     println!("inserted_user:\n{inserted_user:?}");
 }
 
+fn batch_insert(connection: &mut SqliteConnection) {
+    use self::schema::users::dsl::*;
+
+    // let values = &vec![name.eq("Sean"), name.eq("Tess")];
+    // let values = &vec![Some(name.eq("Sean")), None];
+    // let values = &vec![
+    //     (name.eq("Sean"), hair_color.eq("Black")),
+    //     (name.eq("Tess"), hair_color.eq("Brown")),
+    // ];
+    // let values = &vec![
+    //     (name.eq("Sean"), Some(hair_color.eq("Black"))),
+    //     (name.eq("Ruby"), None),
+    // ];
+
+    let json = r#"[
+        { "name": "Sean", "hair_color": "Black" },
+        { "name": "Tess", "hair_color": "Brown" }
+    ]"#;
+    let user_form = serde_json::from_str::<Vec<UserForm>>(json)
+    .expect("failed to parse UserForm");
+
+    let query = diesel::insert_into(users)
+        .values(&user_form);
+    println!("query:\n{}",debug_query::<Sqlite, _>(&query));
+    let inserted_users = query
+        .get_results::<User>(connection)
+        .expect("failed to insert users");
+    println!("inserted_users:\n{inserted_users:?}");
+}   
+
 fn main() {
   let connection = &mut establish_connection();
     // insert_defaults(connection);
     // insert_user_with_name(connection, "Sean");
     // insert_user_with_name_and_haircolor(connection, "Tess", "Brown");
-    insert_with_insertable(connection);
+    // insert_with_insertable(connection);
+    batch_insert(connection);
 }

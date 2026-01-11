e2ecb348e0882a99a96184e64fa6eca0e9e19bd8
spaceofin <80182521+spaceofin@users.noreply.github.com>
2026-01-11 14:26:55 +0900

    [rust-basics]feat: update oop module to compare generics with trait objects

--- rust-basics/basics/src/rust_lang/oop.rs
+++ rust-basics/basics/src/rust_lang/oop.rs
@@ -61,17 +61,17 @@ fn encapsulation_example() {
     println!("collection: {collection:?}");
 }
 
+
 mod gui {
     pub trait Draw {
         fn draw(&self);
     }
 
     pub struct Screen {
-        // Trait objects are created by specifying a pointer, such as a reference
+        // Trait objects are created by specifying a pointer, such as a reference 
         // or a Box<T> smart pointer, followed by the 'dyn' keyword and the trait name.
         pub components: Vec<Box<dyn Draw>>,
     }
-
     impl Screen {
         pub fn run(&self) {
             for component in self.components.iter() {
@@ -93,6 +93,21 @@ mod gui {
                 self.width, self.height, self.label
             );
             // code to actually draw a button
+        }
+    }
+
+    pub struct StaticScreen<T: Draw> {
+        pub components: Vec<T>,
+    }
+
+    impl<T> StaticScreen<T>
+    where
+        T: Draw,
+    {
+        pub fn run(&self) {
+            for component in self.components.iter() {
+                component.draw();
+            }
         }
     }
 }
@@ -144,8 +159,68 @@ fn trait_objects_example() {
     // screen.run();
 }
 
+fn generics_example() {
+    use gui::{Button, StaticScreen, Draw};
+
+    struct SelectBox {
+        width: u32,
+        height: u32,
+        options: Vec<String>,
+    }
+
+    impl Draw for SelectBox {
+        fn draw(&self) {
+            println!(
+            "Drawing SelectBox (width: {}, height: {}, options: {:?})",
+            self.width, self.height, self.options
+        );
+            // code to actually draw a select box
+        }
+    }
+
+    let screen1 = StaticScreen {
+        components: vec![
+            SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            },
+            SelectBox {
+                width: 100,
+                height: 20,
+                options: vec![
+                    String::from("No"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            },
+        ],
+    };
+    screen1.run();
+
+    let screen2 = StaticScreen {
+        components: vec![
+            Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            },
+            Button {
+                width: 70,
+                height: 15,
+                label: String::from("CANCEL"),
+            },
+        ],
+    };
+    screen2.run();
+}
 
 pub fn run() {
     // encapsulation_example();
-    trait_objects_example();
-}
+    // trait_objects_example();
+    generics_example();
+}   

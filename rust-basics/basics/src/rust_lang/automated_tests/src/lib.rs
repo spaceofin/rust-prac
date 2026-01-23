76aa4dba5381b1f8c383a932bf611d91c217f408
spaceofin <80182521+spaceofin@users.noreply.github.com>
2026-01-23 16:25:08 +0900

    [rust-basics]feat: update automated_tests package for assertion and panic test exercises

--- rust-basics/basics/src/rust_lang/automated_tests/src/lib.rs
+++ rust-basics/basics/src/rust_lang/automated_tests/src/lib.rs
@@ -4,6 +4,18 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn add_two(a: u64) -> u64 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {name}!")
+}
+
+// pub fn greeting(name: &str) -> String {
+//     String::from("Hello!")
+// }
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -14,6 +26,38 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {value}."
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {value}."
+            );
+        }
+        Guess { value }
+    }
+
+    // pub fn new(value: i32) -> Guess {
+    //     if value < 1 {
+    //         panic!(
+    //             "Guess value must be less than or equal to 100, got {value}."
+    //         );
+    //     } else if value > 100 {
+    //         panic!(
+    //             "Guess value must be greater than or equal to 1, got {value}."
+    //         );
+    //     }
+    //     Guess { value }
+    // }
 }
 
 #[cfg(test)]
@@ -31,7 +75,7 @@ mod tests {
     //     panic!("Make this test fail");
     // }
 
-        #[test]
+    #[test]
     fn larger_can_hold_smaller() {
         let larger = Rectangle {
             width: 8,
@@ -45,7 +89,7 @@ mod tests {
         assert!(larger.can_hold(&smaller));
     }
 
-        #[test]
+    #[test]
     fn smaller_cannot_hold_larger() {
         let larger = Rectangle {
             width: 8,
@@ -57,6 +101,52 @@ mod tests {
         };
 
         assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{result}`"
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
+        // let result = add(2, 3);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    fn parse_ok() -> Result<(), std::num::ParseIntError> {
+        let n: i32 = "42".parse()?;
+        assert_eq!(n, 42);
+        Ok(())
+    }
+
+    #[test]
+    fn parse_err() {
+        let result: Result<i32, _> = "aa".parse();
+        assert!(result.is_err());
     }
 }
 

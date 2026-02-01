d4c484bc1075a4ab0e41dcb13f273507859e7be8
spaceofin <80182521+spaceofin@users.noreply.github.com>
2026-02-01 20:54:05 +0900

    [rust-basics]feat: update advanced_features module for method disambiguation exercises

--- rust-basics/basics/src/rust_lang/advanced_features.rs
+++ rust-basics/basics/src/rust_lang/advanced_features.rs
@@ -223,6 +223,76 @@ fn operator_overloading() {
     // let _ = Millimeters(1000) + Millimeters(2000);
 }
 
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+struct Cat;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
+impl Animal for Cat {
+    fn baby_name() -> String {
+        String::from("kitten")
+    }
+}
+
+
+fn identically_named_methods() {
+    let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly();
+
+    println!("A baby dog is called a {}", Dog::baby_name());
+    // Compile Error:
+    // println!("A baby dog is called a {}", Animal::baby_name());
+    
+    // Disambiguating between identically named methods
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+    println!("A baby cat is called a {}", <Cat as Animal>::baby_name());
+}
+
+
 pub fn run() {
     // raw_pointers();
     // unsafe_code();
@@ -230,5 +300,6 @@ pub fn run() {
     // call_from_c();
     // global_variables();
     // associated_types_demo();
-    operator_overloading();
+    // operator_overloading();
+    identically_named_methods();
 }

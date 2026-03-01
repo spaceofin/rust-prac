c6a0a51cac8783e59c23c8c09d8213effd107ca0
spaceofin <80182521+spaceofin@users.noreply.github.com>
2026-03-01 19:15:32 +0900

    [rust-libs-practice]feat: setup database connection and auto-migration

--- rust-libs-practice/seaorm-demo/seaorm-app/src/main.rs
+++ rust-libs-practice/seaorm-demo/seaorm-app/src/main.rs
@@ -1,5 +1,22 @@
 mod entities;
 
-fn main() {
-    println!("Hello, SeaORM!");
+use dotenvy::dotenv;
+use migration::{Migrator, MigratorTrait};
+use sea_orm::Database;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), sea_orm::DbErr> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL")
+        .map_err(|_| sea_orm::DbErr::Custom("DATABASE_URL must be set".into()))?;
+
+    let connection = Database::connect(database_url).await?;
+    println!("Database Connected!");
+
+    println!("Running migrations...");
+    Migrator::up(&connection, None).await?;
+    println!("Done");
+
+    Ok(())
 }

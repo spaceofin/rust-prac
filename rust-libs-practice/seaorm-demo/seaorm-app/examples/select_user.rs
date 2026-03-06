18537d775b262be6eb4826ddebed8114cc48325d
spaceofin <80182521+spaceofin@users.noreply.github.com>
2026-03-06 17:58:07 +0900

    [rust-libs-practice]feat: add more select examples for users

--- rust-libs-practice/seaorm-demo/seaorm-app/examples/select_user.rs
+++ rust-libs-practice/seaorm-demo/seaorm-app/examples/select_user.rs
@@ -1,4 +1,4 @@
-use sea_orm::{DatabaseConnection, DbErr, EntityTrait};
+use sea_orm::{DatabaseConnection, DbErr, EntityTrait, QueryFilter, QueryOrder};
 use seaorm_app::entities::users;
 use seaorm_app::establish_connection;
 
@@ -11,11 +11,42 @@ async fn select_first_user(conn: &DatabaseConnection) -> Result<users::Model, Db
     Ok(user)
 }
 
+async fn select_user_by_id(conn: &DatabaseConnection, id: i64) -> Result<users::Model, DbErr> {
+    let user = users::Entity::find_by_id(id)
+        .one(conn)
+        .await?
+        .ok_or_else(|| DbErr::RecordNotFound("No user found".to_owned()))?;
+
+    Ok(user)
+}
+
+async fn select_users_by_filter(conn: &DatabaseConnection) -> Result<Vec<users::Model>, DbErr> {
+    let users: Vec<users::Model> = users::Entity::find()
+        .filter(users::COLUMN.username.contains("o"))
+        .order_by_asc(users::COLUMN.username)
+        .all(conn)
+        .await?;
+
+    Ok(users)
+}
+
 #[tokio::main]
 async fn main() {
     let conn = establish_connection().await.unwrap();
 
-    let selected_user = select_first_user(&conn).await;
+    // let selected_user = select_first_user(&conn).await;
+    // match selected_user {
+    //     Ok(user) => println!("Selected user:\n{:?}", user),
+    //     Err(e) => eprintln!("Select failed:\n{:?}", e),
+    // }
+
+    // let selected_users = select_users_by_filter(&conn).await;
+    // match selected_users {
+    //     Ok(users) => println!("Selected users:\n{:#?}", users),
+    //     Err(e) => eprintln!("Select failed:\n{:?}", e),
+    // }
+
+    let selected_user = select_user_by_id(&conn, 3).await;
     match selected_user {
         Ok(user) => println!("Selected user:\n{:?}", user),
         Err(e) => eprintln!("Select failed:\n{:?}", e),

8092a0c98131bfac0b0b7fb2ee7253c573e7c2fc
spaceofin <80182521+spaceofin@users.noreply.github.com>
2026-03-10 23:51:08 +0900

    [rust-libs-practice]feat: add delete examples for users

--- rust-libs-practice/seaorm-demo/seaorm-app/examples/delete_user.rs
+++ rust-libs-practice/seaorm-demo/seaorm-app/examples/delete_user.rs
@@ -1,4 +1,6 @@
-use sea_orm::{DatabaseConnection, DbErr, DeleteResult, EntityTrait, ModelTrait};
+use sea_orm::{
+    ColumnTrait, DatabaseConnection, DbErr, DeleteResult, EntityTrait, ModelTrait, QueryFilter,
+};
 use seaorm_app::entities::users;
 use seaorm_app::establish_connection;
 
@@ -12,11 +14,58 @@ async fn delete_user(conn: &DatabaseConnection, id: i64) -> Result<DeleteResult,
     Ok(res)
 }
 
+async fn delete_user_direct(conn: &DatabaseConnection, id: i64) -> Result<DeleteResult, DbErr> {
+    let res = users::Entity::delete_by_id(id).exec(conn).await?;
+
+    Ok(res)
+}
+
+async fn delete_user_returning(
+    conn: &DatabaseConnection,
+    id: i64,
+) -> Result<Option<users::Model>, DbErr> {
+    let deleted_user = users::Entity::delete_by_id(id)
+        .exec_with_returning(conn)
+        .await?;
+
+    Ok(deleted_user)
+}
+
+async fn delete_users_by_pattern(
+    conn: &DatabaseConnection,
+    pattern: &str,
+) -> Result<DeleteResult, DbErr> {
+    let res = users::Entity::delete_many()
+        .filter(users::Column::Username.contains(pattern))
+        .exec(conn)
+        .await?;
+
+    Ok(res)
+}
+
 #[tokio::main]
 async fn main() {
     let conn = establish_connection().await.unwrap();
 
-    let res = delete_user(&conn, 2).await;
+    // let res = delete_user(&conn, 2).await;
+    // match res {
+    //     Ok(res) => println!("Delete result:\n{:?}", res),
+    //     Err(e) => eprintln!("Delete failed:\n{:?}", e),
+    // }
+
+    // let res = delete_user_direct(&conn, 6).await;
+    // match res {
+    //     Ok(res) => println!("Delete result:\n{:?}", res),
+    //     Err(e) => eprintln!("Delete failed:\n{:?}", e),
+    // }
+
+    // let deleted_user = delete_user_returning(&conn, 6).await;
+    // match deleted_user {
+    //     Ok(user) => println!("Delete user:\n{:?}", user),
+    //     Err(e) => eprintln!("Delete failed:\n{:?}", e),
+    // }
+
+    let res = delete_users_by_pattern(&conn, "ee").await;
     match res {
         Ok(res) => println!("Delete result:\n{:?}", res),
         Err(e) => eprintln!("Delete failed:\n{:?}", e),
